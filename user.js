6f07620e67410c67b321ccbffd78f985478c547c
honbag <lightpunch@gmail.com>
2017-04-08 02:03:12 -0400

    Update user.js

--- user.js
+++ user.js
@@ -11,7 +11,7 @@
 
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
-script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/honbag/honbagat2/AutoTrimps2.js';
 document.head.appendChild(script);
 
 // This File is Deprecated, please use .user.js instead (with the leading dot) so TamperMonkey/Greasemonkey can detect it as a script automatically.
